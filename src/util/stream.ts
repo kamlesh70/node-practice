64b3765036dcb9433f5eb73146fa565897d27eab
kamlesh70 <kamalmehra3571@gmail.com>
2025-01-04 18:40:14 +0530

    implement readable, writable, transform and pipeline in stream

--- src/util/stream.ts
+++ src/util/stream.ts
@@ -3,6 +3,7 @@
 */
 import fs from "fs";
 import path from "path";
+import { pipeline, Readable, Transform, Writable } from "stream";
 
 export function streams() {
   const readStream = fs.createReadStream(
@@ -33,5 +34,41 @@ export function streams() {
 
   readStream.on("end", function () {
     console.log("Finished reading file");
+  });
+
+  const readableStream = new Readable({
+    objectMode: true, // object mode is used when reading object
+    read() {},
+  });
+
+  const writable = new Writable({
+    objectMode: true,
+    write(chunk, encoding, callback) {
+      // writable.emit("error", new Error("testing error "));
+      console.log("Writing to file:", chunk);
+      callback(null);
+    },
+  });
+
+  readableStream.push({ test: "hello!" });
+
+  const transform = new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      if (typeof chunk == "object" && "test" in chunk) {
+        chunk.test = "transformed";
+      }
+      callback(null, chunk);
+    },
+  });
+
+  // pipe is used to connect streams but if any stream fail it doesn't close all the streams. We can use pipeline instead.
+  // readableStream.pipe(transform).pipe(writeStream);
+  pipeline(readableStream, transform, writable, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("stream finished");
+    }
   });
 }
