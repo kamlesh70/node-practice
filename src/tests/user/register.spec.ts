745d3301e078ddfe3f398b515157cbe3c92b9706
kamlesh70 <kamalmehra3571@gmail.com>
2025-02-26 22:31:46 +0530

    add test coverage

--- src/tests/user/register.spec.ts
+++ src/tests/user/register.spec.ts
@@ -3,19 +3,17 @@ import { app } from '../..';
 
 describe("testing /user/register", () => {
   // happy path
-  describe("Test cases having all the required fields", () => { 
+  describe("Test cases having all the required fields", () => {
     it("should return 201 status code", async () => {
       const user = {
         name: "kamlesh",
         email: "kamlesh@gmail.com",
-      }
-  
-      const response = await request(app).post('/user/register').send(user);
-      expect(response.statusCode).toBe(201);
-    })
-  })
+      };
 
+      const response = await request(app).post("/user/register").send(user);
+      expect(response.statusCode).toBe(201);
+    });
+  });
 
   // Sad Path
-
 })
